5a93f399351cfb6f92ad844adc02249023c957d9
stefan <segemnay@edu.uwaterloo.ca>
2019-06-22 15:42:16 -0400

    launch quiz button-press works

--- app/src/main/java/com/flamingo/wikiquiz/LaunchQuizFragment.java
+++ app/src/main/java/com/flamingo/wikiquiz/LaunchQuizFragment.java
@@ -1,17 +1,16 @@
 package com.flamingo.wikiquiz;
 
 
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.navigation.Navigation;
 
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class LaunchQuizFragment extends Fragment {
 
 
@@ -24,7 +23,12 @@ public class LaunchQuizFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_launch_quiz, container, false);
+        View view = inflater.inflate(R.layout.fragment_launch_quiz, container, false);
+
+        Button button = view.findViewById(R.id.launchQuizButton);
+        button.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_launchQuizFragment_to_questionFragment, null));
+
+        return view;
     }
 
 }
