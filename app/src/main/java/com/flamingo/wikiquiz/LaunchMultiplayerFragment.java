91161fe91d9426d4698a50db40a871f83453a21e
Caio Souza <caio.souza@uwaterloo.ca>
2019-07-19 19:15:34 -0400

    Commit to test connectivity.

--- app/src/main/java/com/flamingo/wikiquiz/LaunchMultiplayerFragment.java
+++ app/src/main/java/com/flamingo/wikiquiz/LaunchMultiplayerFragment.java
@@ -12,6 +12,7 @@ import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -28,6 +29,8 @@ import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.UUID;
 
 public class LaunchMultiplayerFragment extends Fragment {
@@ -36,11 +39,13 @@ public class LaunchMultiplayerFragment extends Fragment {
     private BluetoothAdapter bluetoothAdapter;
     private ArrayAdapter<String> deviceNamesAdapter;
 
+    private AcceptThread mAcceptThread;
+    private ConnectThread mConnectThread;
+    private ConnectedThread mConnectedThread;
 
-    //TODO maybe delete
-    //private ConnectedThread connectedThread; // bluetooth background worker thread to send and receive data
-    //private BluetoothSocket btSocket = null; // bi-directional client-to-client data path
-
+    public static final int MESSAGE_READ = 1;
+    public static final int MESSAGE_WRITE = 2;
+    public static final int MESSAGE_TOAST = 3;
 
     public LaunchMultiplayerFragment() {
         // Required empty constructor
@@ -52,8 +57,8 @@ public class LaunchMultiplayerFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_launch_multiplayer, container, false);
 
-        deviceNamesAdapter = new ArrayAdapter<String> (getContext(), android.R.layout.simple_list_item_1);
-        bluetoothAdapter =  BluetoothAdapter.getDefaultAdapter();
+        deviceNamesAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1);
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
         ListView devicesListView = view.findViewById(R.id.devicesListView);
         devicesListView.setAdapter(deviceNamesAdapter);
@@ -61,28 +66,27 @@ public class LaunchMultiplayerFragment extends Fragment {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
 
-                //
-//                if(!bluetoothAdapter.isEnabled()) {
-//                    Toast.makeText(getContext(), "Bluetooth not on", Toast.LENGTH_SHORT).show();
-//                    return;
-//                }
-//
-           //  mBluetoothStatus.setText("Connecting...");
-               // Get the device MAC address, which is the last 17 chars in the View
-               String info = ((TextView) view).getText().toString();
-              final String address = info.substring(info.length() - 17);
-              final String name = info.substring(0,info.length() - 17);
-//
-//                // Spawn a new thread to avoid blocking the GUI one
-//                new Thread()
-//                {
-//                    public void run() {
-//                        boolean fail = false;
-//
-//                        BluetoothDevice device = bluetoothAdapter.getRemoteDevice(address);
-//
+                if (!bluetoothAdapter.isEnabled()) {
+                    Toast.makeText(getContext(), "Bluetooth not on", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                // Get the device MAC address, which is the last 17 chars in the View
+                String info = ((TextView) view).getText().toString();
+                final String address = info.substring(info.length() - 17);
+                final String name = info.substring(0, info.length() - 17);
+
+                // Spawn a new thread to avoid blocking the GUI one
+                new Thread() {
+                    public void run() {
+                        boolean fail = false;
+
+                        BluetoothDevice device = bluetoothAdapter.getRemoteDevice(address);
+
+                        mConnectThread = new ConnectThread(device);
+                        mConnectThread.run();
 //                        try {
-//                          btSocket  = createBluetoothSocket(device);
+//                            btSocket = createBluetoothSocket(device);
 //                        } catch (IOException e) {
 //                            fail = true;
 //                            Toast.makeText(getContext(), "Socket creation failed", Toast.LENGTH_SHORT).show();
@@ -101,22 +105,19 @@ public class LaunchMultiplayerFragment extends Fragment {
 //                                Toast.makeText(getContext(), "Socket creation failed", Toast.LENGTH_SHORT).show();
 //                            }
 //                        }
-//                        if(fail == false) {
+//                        if (fail == false) {
 //                            mConnectedThread = new ConnectedThread(btSocket);
 //                            mConnectedThread.start();
 //
 //                            handler.obtainMessage(CONNECTING_STATUS, 1, -1, name)
 //                                    .sendToTarget();
 //                        }
-//                    }
-//                }.start();
-//            }
-//
-//            }
-    }});
+                    }
+                }.start();
+            }
+        });
 
         Button hostBtn = view.findViewById(R.id.hostSession);
-
         hostBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -126,10 +127,9 @@ public class LaunchMultiplayerFragment extends Fragment {
                     discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 120);
                     startActivity(discoverableIntent);
                     Toast.makeText(getContext(), "Device now discoverable", Toast.LENGTH_SHORT).show();
-                    AcceptThread acceptThread = new AcceptThread();
-                    acceptThread.run();
-                }
-                else {
+                    mAcceptThread = new AcceptThread();
+                    mAcceptThread.run();
+                } else {
                     // Device does not support bluetooth
                 }
             }
@@ -138,7 +138,6 @@ public class LaunchMultiplayerFragment extends Fragment {
         if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 56789);
         }
-//        int permissionCheck = ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION);
 
         Button joinBtn = view.findViewById(R.id.joinSession);
         joinBtn.setOnClickListener(new View.OnClickListener() {
@@ -153,13 +152,13 @@ public class LaunchMultiplayerFragment extends Fragment {
                     getActivity().getApplicationContext().registerReceiver(receiver, new IntentFilter(BluetoothDevice.ACTION_FOUND));
 
                     Toast.makeText(getContext(), "Began discovery", Toast.LENGTH_SHORT).show();
-                    //ConnectThread connectThread = new ConnectThread()
-                }
-                else {
+                } else {
                     // Device does not support bluetooth
                 }
             }
         });
+
+
 
         return view;
     }
@@ -187,6 +186,34 @@ public class LaunchMultiplayerFragment extends Fragment {
         }
     };
 
+    private final Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case MESSAGE_READ:
+                    byte[] readBuf = (byte[]) msg.obj;
+                    // construct a string from the valid bytes in the buffer
+                    String readMessage = new String(readBuf, 0, msg.arg1);
+                    Toast.makeText(getActivity(), "Received Message: " + readMessage,
+                            Toast.LENGTH_SHORT).show();
+                    break;
+                case MESSAGE_WRITE:
+                    byte[] writeBuf = (byte[]) msg.obj;
+                    // construct a string from the buffer
+                    String writeMessage = new String(writeBuf);
+                    Toast.makeText(getActivity(), "Sent Message: " + writeMessage,
+                            Toast.LENGTH_SHORT).show();
+                    break;
+                case MESSAGE_TOAST:
+                    if (getActivity() != null) {
+                        Toast.makeText(getActivity(), msg.getData().getString("failed"),
+                                Toast.LENGTH_SHORT).show();
+                    }
+                    break;
+            }
+        }
+    };
+
     public boolean checkEnableBluetooth() {
         int REQUEST_ENABLE_BT = 1234;
         if (bluetoothAdapter == null) {
@@ -200,18 +227,30 @@ public class LaunchMultiplayerFragment extends Fragment {
         return true;
     }
 
-//    public BluetoothDevice findBluetoothDevice() {
-//        BluetoothDevice bluetoothDevice;
-//        Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
-//        if (pairedDevices.size() > 0) {
-//            for (BluetoothDevice device : pairedDevices) {
-//                String deviceName = device.getName();
-//                String deviceHA = device.getAddress();
-//                bluetoothDevice = device;
-//            }
-//        }
-//        return bluetoothDevice;
-//    }
+    private synchronized void manageMyConnectedSocket(BluetoothSocket socket,
+                                                      BluetoothDevice device) {
+        // Cancel the thread that completed the connection
+        if (mConnectThread != null) {
+            mConnectThread.cancel();
+            mConnectThread = null;
+        }
+
+        // Cancel any thread currently running a connection
+        if (mConnectedThread != null) {
+            mConnectedThread.cancel();
+            mConnectedThread = null;
+        }
+
+        //Cancel accept thread because we only want to connect to one device
+        if (mAcceptThread != null) {
+            mAcceptThread.cancel();
+            mAcceptThread= null;
+        }
+
+        // Start the thread to manage the connection and perform transmissions
+        mConnectedThread = new ConnectedThread(socket);
+        mConnectedThread.start();
+    }
 
     private class AcceptThread extends Thread {
         private final BluetoothServerSocket mmServerSocket;
@@ -239,9 +278,11 @@ public class LaunchMultiplayerFragment extends Fragment {
                 }
 
                 if (socket != null) {
-                    //manageMyConnectedSocket(socket);
-                    cancel();
-                    break;
+                    synchronized (LaunchMultiplayerFragment.this) {
+                        manageMyConnectedSocket(socket, socket.getRemoteDevice());
+                        cancel();
+                        break;
+                    }
                 }
             }
         }
@@ -281,7 +322,11 @@ public class LaunchMultiplayerFragment extends Fragment {
                 Log.e("ConnectThread", "Socket's connect() method failed", connectException);
                 return;
             }
-            //manageMyConnectedSocket(mmSocket);
+
+            synchronized (LaunchMultiplayerFragment.this) {
+                mConnectThread = null;
+            }
+            manageMyConnectedSocket(mmSocket, mmDevice);
         }
 
         public void cancel() {
@@ -289,6 +334,88 @@ public class LaunchMultiplayerFragment extends Fragment {
                 mmSocket.close();
             } catch (IOException closeException) {
                 Log.e("ConnectThread", "Could not close the client socket", closeException);
+            }
+        }
+    }
+
+    private class ConnectedThread extends Thread {
+        private final BluetoothSocket mmSocket;
+        private final InputStream mmInStream;
+        private final OutputStream mmOutStream;
+        private byte[] mmBuffer; // mmBuffer store for the stream
+
+        public ConnectedThread(BluetoothSocket socket) {
+            mmSocket = socket;
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
+
+            // Get the input and output streams; using temp objects because
+            // member streams are final.
+            try {
+                tmpIn = socket.getInputStream();
+            } catch (IOException e) {
+                Log.e("ConnectedThread", "Error occurred when creating input stream", e);
+            }
+            try {
+                tmpOut = socket.getOutputStream();
+            } catch (IOException e) {
+                Log.e("ConnectedThread", "Error occurred when creating output stream", e);
+            }
+
+            mmInStream = tmpIn;
+            mmOutStream = tmpOut;
+        }
+
+        public void run() {
+            mmBuffer = new byte[1024];
+            int numBytes; // bytes returned from read()
+
+            // Keep listening to the InputStream until an exception occurs.
+            while (true) {
+                try {
+                    // Read from the InputStream.
+                    numBytes = mmInStream.read(mmBuffer);
+                    // Send the obtained bytes to the UI activity.
+                    Message readMsg = handler.obtainMessage(
+                            MESSAGE_READ, numBytes, -1,
+                            mmBuffer);
+                    readMsg.sendToTarget();
+                } catch (IOException e) {
+                    Log.d("ConnectedThread", "Input stream was disconnected", e);
+                    break;
+                }
+            }
+        }
+
+        // Call this from the main activity to send data to the remote device.
+        public void write(byte[] bytes) {
+            try {
+                mmOutStream.write(bytes);
+
+                // Share the sent message with the UI activity.
+                Message writtenMsg = handler.obtainMessage(
+                        MESSAGE_WRITE, -1, -1, mmBuffer);
+                writtenMsg.sendToTarget();
+            } catch (IOException e) {
+                Log.e("ConnectedThread", "Error occurred when sending data", e);
+
+                // Send a failure message back to the activity.
+                Message writeErrorMsg =
+                        handler.obtainMessage(MESSAGE_TOAST);
+                Bundle bundle = new Bundle();
+                bundle.putString("failed",
+                        "Couldn't send data to the other device");
+                writeErrorMsg.setData(bundle);
+                handler.sendMessage(writeErrorMsg);
+            }
+        }
+
+        // Call this method from the main activity to shut down the connection.
+        public void cancel() {
+            try {
+                mmSocket.close();
+            } catch (IOException e) {
+                Log.e("ConnectedThread", "Could not close the connect socket", e);
             }
         }
     }
